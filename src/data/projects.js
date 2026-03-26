989e2afe4887c037e69344500b7c56ff9f35ed90
Victoria Acuña <victoria.acuna2709@gmail.com>
2026-03-26 20:33:50 -0300

    agrego nuevos proyectos al data

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,21 +2,24 @@
     import smartTravel from '@/assets/img/smart_travel.png'
     import dailyWeather from '@/assets/img/daily-weather.png'
     import cuppon from '@/assets/img/cuppon.png'
+    import rickmortyAPI from '@/assets/img/rick-morty-API.png'
+    import wwapp from '@/assets/img/wwapp.png'
+    import movieNight from '@/assets/img/movie-night.png'
 
 export const projects = [
         {
             title: 'RESTAURANTCITO',
             description: 'Plataforma interactiva para la gestion de pedidos y reservas.',
             image: restaurancito,
-            repo: 'https://github.com/VictoriaAcunaParedes/restaurant',
+            repo: 'https://github.com/VictoriaAcunaParedes/restaurant-vue',
             demo: '#',
-            tags: ['HTML5', 'CSS3', 'Bootstrap 5']
+            tags: ['HTML5', 'CSS3', 'Bootstrap 5', 'Vue 3']
         },
         {
             title: 'SMART TRAVEL',
             description: 'Landing page para agencia de viajes con diseño responsivo',
             image: smartTravel,
-            repo: '#',
+            repo: 'https://github.com/VictoriaAcunaParedes/smart-travel',
             demo: '#',
             tags: ['HTML5', 'CSS3', 'Bootstrap 5']
         },
@@ -29,11 +32,35 @@ export const projects = [
             tags: ['HTML5', 'CSS3', 'Bootstrap 5', 'JavaScript']
         },
         {
-            title: 'CUPPON',
-            description: 'E-commerce de cupones con maquetacion avanzada.',
-            image: cuppon,
-            repo: 'https://github.com/VictoriaAcunaParedes/tienda-cuppon',
+            title: 'RICK & MORTY',
+            description: 'Aplicacion de personajes de serie con consumo de API.',
+            image: rickmortyAPI,
+            repo: 'https://github.com/VictoriaAcunaParedes/rick-and-morty-api',
             demo: '#',
-            tags: ['HTML5', 'CSS3', 'Bootstrap 5']
-        }
+            tags: ['HTML5', 'CSS3', 'Bootstrap 5', 'JavaScript', 'API']
+        },
+        {
+            title: 'W W App',
+            description: 'App desarrollada con Vue.js consumiendo datos en tiempo real desde Open-Meteo API',
+            image: wwapp,
+            repo: 'https://github.com/VictoriaAcunaParedes/world-weather-app',
+            demo: '#',
+            tags: ['HTML5', 'CSS3', 'Bootstrap 5', 'JavaScript', 'API']
+        },
+        {
+            title: 'Movie Night',
+            description: 'App desarrollada con Vue.js + database de Firebase para almacenaciemto de data peliculas',
+            image: movieNight,
+            repo: 'https://github.com/VictoriaAcunaParedes/Movie-Night',
+            demo: '#',
+            tags: ['HTML5', 'CSS3', 'Bootstrap 5', 'JavaScript', 'Firebase']
+        },
+        // {
+        //     title: 'CUPPON',
+        //     description: 'E-commerce de cupones con maquetacion avanzada.',
+        //     image: cuppon,
+        //     repo: 'https://github.com/VictoriaAcunaParedes/tienda-cuppon',
+        //     demo: '#',
+        //     tags: ['HTML5', 'CSS3', 'Bootstrap 5']
+        // }
     ]
